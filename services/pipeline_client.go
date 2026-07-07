d81ef3703218e5b4ac83093955e4ca80557836c9
Fu Gui <szfugui@gmail.com>
2026-07-07 16:59:20 +0800

    fix: 修复删除检查任务时 payload 被二次序列化成 base64 的问题

--- services/pipeline_client.go
+++ services/pipeline_client.go
@@ -759,16 +759,11 @@ func SyncDeleteExecutionSchemeRemote(scheme models.ExecutionScheme, headers map[
 			payload := map[string]interface{}{
 				"taskIds": []string{scheme.CodeCheckerTaskID},
 			}
-			postData, err := json.Marshal(payload)
-			if err == nil {
-				_, err = utils.SendHTTPRequest(context.Background(), "DELETE", apiURLStr, postData, utils.HTTPOptions{
-					Headers: headers,
-				}, []int{http.StatusOK, http.StatusNoContent, http.StatusAccepted}, "SyncDeleteCheckerTask")
-				if err != nil {
-					log.Printf("[SyncDelete] Failed to delete checker task %s: %v\n", scheme.CodeCheckerTaskID, err)
-				}
-			} else {
-				log.Printf("[SyncDelete] Failed to marshal delete checker task payload: %v\n", err)
+			_, err := utils.SendHTTPRequest(context.Background(), "DELETE", apiURLStr, payload, utils.HTTPOptions{
+				Headers: headers,
+			}, []int{http.StatusOK, http.StatusNoContent, http.StatusAccepted}, "SyncDeleteCheckerTask")
+			if err != nil {
+				log.Printf("[SyncDelete] Failed to delete checker task %s: %v\n", scheme.CodeCheckerTaskID, err)
 			}
 		}
 	}
